183fbec3b1952b691ff6089b02aa0274c357d0be
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-05-03 09:18:03 +0200

    Add Model skeleton

---
+++ src/main/java/com/codecool/web/model/Schedule.java
@@ -0,0 +1,30 @@
+package com.codecool.web.model;
+
+public class Schedule {
+
+    private int id;
+    private String name;
+    private boolean isPublic;
+
+    public Schedule(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.isPublic = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic() {
+        isPublic = true;
+    }
+}
