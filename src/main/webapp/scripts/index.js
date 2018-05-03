d672625f0534816754651631dea4184350e39a5e
Gyorfisti <gyorfisti@gmail.com>
2018-05-03 13:21:29 +0200

    added 2 functions to index.js

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,3 +1,21 @@
+function showContents(ids) {
+    const contentEls = document.getElementsByClassName('content');
+    for (let i = 0; i < contentEls.length; i++) {
+        const contentEl = contentEls[i];
+        if (ids.includes(contentEl.id)) {
+            contentEl.classList.remove('hidden');
+        } else {
+            contentEl.classList.add('hidden');
+        }
+    }
+}
+
+function removeAllChildren(el) {
+    while (el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+}
+
 function onLoad() {
     document.getElementById('login-content').classList.remove('hidden');
     document.getElementById('register-content').classList.add('hidden');
