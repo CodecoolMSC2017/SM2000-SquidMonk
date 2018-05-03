64f4783c092ed3cbec7f0fbbd512aaec3c85ae8d
Gyorfisti <gyorfisti@gmail.com>
2018-05-03 14:00:14 +0200

    changed IllegalArgumentException to ServiceException

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.codecool.web.dao;
 
 import com.codecool.web.model.User;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 
@@ -8,7 +9,7 @@ public interface UserDao {
 
     User findById(int id) throws SQLException;
 
-    User findByEmail(String email) throws SQLException;
+    User findByEmail(String email) throws SQLException, ServiceException;
 
     User insertUser(String name, String email, String password) throws SQLException;
 
