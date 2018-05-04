5b5e84819a8f19560645f2e875a86133d3420c68
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-04 09:04:54 +0200

    Connector class still WIP!

--- src/main/java/com/codecool/web/dao/implementation/TskColSchedConnectorDao.java
+++ src/main/java/com/codecool/web/dao/implementation/TskColSchedConnectorDao.java
@@ -36,11 +36,14 @@ class TskColSchedConnectorDao extends AbstractDao {
             executeInsert(statement);
         }
     }
-    /*
-    TODO: You have to select and get the data for the following first, in case you don't update everything.
-    TODO: For this you will be needing a control object which can hold the data.
-     */
-    void updateTask(int taskId, int colId, int scheduleId, int taskStart, int taskEnd) throws SQLException {
+
+    //Work In Progress!
+
+    void updateTask(int taskId, String colId, String scheduleId, String taskStart, String taskEnd) throws SQLException {
+        /*ConnectorDataTransfer connectorDataTransfer = queryTaskAll(taskId);
+
+        if (colId.equals(""))
+
         String sql = "UPDATE col_tsk SET col_id=?, schedule_id=?, task_start=?, task_end=? WHERE task_id=?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, taskId);
@@ -49,6 +52,60 @@ class TskColSchedConnectorDao extends AbstractDao {
             statement.setInt(4, taskStart);
             statement.setInt(5, taskEnd);
             executeInsert(statement);
+        }*/
+    }
+
+    private ConnectorDataTransfer queryTaskAll(int taskId) throws SQLException {
+        String sql = "SELECT task_id, col_id, schedule_id, task_start, task_end FROM col_tsk WHERE task_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, taskId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("task_id");
+                    int colId = resultSet.getInt("col_id");
+                    int scheduleId = resultSet.getInt("schedule_id");
+                    int taskStart = resultSet.getInt("task_start");
+                    int taskEnd = resultSet.getInt("task_end");
+
+                    return new ConnectorDataTransfer(id, colId, scheduleId, taskStart, taskEnd);
+                }
+            }
+        }
+        return null;
+    }
+
+    /*Inner class for data transfer of connector table data starts here*/
+
+    private class ConnectorDataTransfer {
+
+        int id, colId, scheduleId, taskStart, taskEnd;
+
+        ConnectorDataTransfer(int id, int colId, int scheduleId, int taskStart, int taskEnd) {
+            this.id = id;
+            this.colId = colId;
+            this.scheduleId = scheduleId;
+            this.taskStart = taskStart;
+            this.taskEnd = taskEnd;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getColId() {
+            return colId;
+        }
+
+        public int getScheduleId() {
+            return scheduleId;
+        }
+
+        public int getTaskStart() {
+            return taskStart;
+        }
+
+        public int getTaskEnd() {
+            return taskEnd;
         }
     }
 }
