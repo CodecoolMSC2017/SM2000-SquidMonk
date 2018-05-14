9b17d7736c24cd2b522b44ac90edc905f512ff2a
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-14 16:03:37 +0200

    Scheduleservlet added, schedule.js now handles menuclick produces table of schedules. ScheduleDto added for data transfer. Incomplete.

--- src/main/webapp/scripts/schedule.js
+++ src/main/webapp/scripts/schedule.js
@@ -1,15 +1,50 @@
 function onScheduleReceived() {
+    const mainDiv = document.getElementById('main-content');
+    const schedule = JSON.parse(this.responseText);
 
+    
 }
 
 function onScheduleClick() {
-    const user = JSON.parse(localStorage.getItem('user'));
-    const params = new URLSearchParams();
-    params.append('poemId', user.id);
-
+    const value = this.getAttribute('value');
+    
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', onScheduleReceived);
     xhr.addEventListener('error', onNetworkError);
-    xhr.open('GET', '/protected/schedules/user/' + params.toString());
+    xhr.open('GET', 'protected/schedule/' + user.id);
+    xhr.send();
+}
+
+function onSchedulesReceived() {
+    const mainDiv = document.getElementById('main-content');
+    removeAllChildren(mainDiv);
+    const schedules = JSON.parse(this.responseText);
+
+    const tableEl = document.createElement('table');
+    const rowEl = document.createElement('tr');
+
+    for (let i = 0; i < schedules.length; i++) {
+        const schedule = schedules[i];
+        const tdEl = document.createElement('td');
+        tdEl.textContent = schedule.name;
+        tdEl.setAttribute('value', schedule.id);
+        tdEl.addEventListener('mouseover', onSchedMouseOver);
+        tdEl.addEventListener('mouseout', onSchedMouseOut);
+        tdEl.addEventListener('click', onScheduleClick);
+
+        rowEl.appendChild(tdEl);
+    }
+
+    tableEl.appendChild(rowEl);
+    mainDiv.appendChild(tableEl);
+}
+
+function onMenuScheduleClick() {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onSchedulesReceived);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/schedules/user/' + user.id);
     xhr.send();
 }
