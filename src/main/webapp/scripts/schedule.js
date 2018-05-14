c1f1be04fb76c4503267356a083e10044afc2d90
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-14 13:53:00 +0200

    script added to index

--- src/main/webapp/scripts/schedule.js
+++ src/main/webapp/scripts/schedule.js
@@ -1,5 +1,5 @@
 function onScheduleReceived() {
-    
+
 }
 
 function onScheduleClick() {
@@ -10,6 +10,6 @@ function onScheduleClick() {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', onScheduleReceived);
     xhr.addEventListener('error', onNetworkError);
-    xhr.open('GET', 'protected/poems?' + params.toString());
+    xhr.open('GET', '/protected/schedules/user/?' + params.toString());
     xhr.send();
 }
