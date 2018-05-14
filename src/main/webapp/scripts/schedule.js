14817ba13c1c3fb048c55283435252b87ecd7e4b
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-14 13:50:38 +0200

    onscheduledclick added

--- src/main/webapp/scripts/schedule.js
+++ src/main/webapp/scripts/schedule.js
@@ -1,3 +1,15 @@
-function onScheduleClick() {
+function onScheduleReceived() {
     
+}
+
+function onScheduleClick() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const params = new URLSearchParams();
+    params.append('poemId', user.id);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onScheduleReceived);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/poems?' + params.toString());
+    xhr.send();
 }
