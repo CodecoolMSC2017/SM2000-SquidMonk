946213610c30a807b717aed596f8018c6f4db6dc
HajnalFeri <kalacsf1@gmail.com>
2018-05-14 14:03:49 +0200

    schedule dao test

--- src/test/java/com/codecool/web/dao/implementation/ScheduleDaoImplTest.java
+++ src/test/java/com/codecool/web/dao/implementation/ScheduleDaoImplTest.java
@@ -52,23 +52,58 @@ class ScheduleDaoImplTest {
     }
 
     @Test
-    void insertSchedule() {
+    void insertSchedule() throws SQLException {
+        try (Connection con = DriverManager.getConnection(dbUrl, "test", "test")) {
+            int userId = 3;
+            String name = "Alexa sched insert test";
+            boolean isPublic = true;
+
+            ScheduleDaoImpl scheduleDao = new ScheduleDaoImpl(con);
+            scheduleDao.insertSchedule(userId, name, isPublic);
+
+            List<Schedule> schedules = new ScheduleDaoImpl(con).findAllByUserId(3);
+            assertEquals(2, schedules.size());
+            assertEquals(8, schedules.get(1).getId());
+            assertEquals(name, schedules.get(1).getName());
+            assertTrue(schedules.get(1).isPublic());
+        }
     }
 
     @Test
-    void updateVisibility() {
+    void updateVisibility() throws SQLException {
+        try (Connection con = DriverManager.getConnection(dbUrl, "test", "test")) {
+            int scheduleId = 1;
+            boolean isPublic = false;
+
+            ScheduleDaoImpl scheduleDao = new ScheduleDaoImpl(con);
+
+            Schedule schedule = scheduleDao.findById(scheduleId);
+            assertTrue(schedule.isPublic());
+
+            scheduleDao.updateVisibility(scheduleId, isPublic);
+            Schedule scheduleUpdateVisibility = scheduleDao.findById(scheduleId);
+            assertFalse(scheduleUpdateVisibility.isPublic());
+        }
     }
 
     @Test
-    void updateName() {
+    void updateName() throws SQLException {
+        try (Connection con = DriverManager.getConnection(dbUrl, "test", "test")) {
+            int scheduleId = 4;
+            String name = "Csba sched updateNameTest";
+
+            ScheduleDaoImpl scheduleDao = new ScheduleDaoImpl(con);
+            Schedule schedule = scheduleDao.findById(scheduleId);
+            assertEquals("Csba sched #2", schedule.getName());
+
+            scheduleDao.updateName(scheduleId, name);
+            Schedule scheduleUpdateNameTest = scheduleDao.findById(scheduleId);
+            assertEquals("Csba sched updateNameTest", scheduleUpdateNameTest.getName());
+        }
     }
 
     @Test
     void deleteSchedule() {
-    }
-
-    @Test
-    void updateScheduleCount() {
     }
 
     void resetDb() throws ClassNotFoundException, SQLException {
