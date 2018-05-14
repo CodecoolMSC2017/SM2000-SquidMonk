29963a862a5d024005f6c0a9775f71dbc83edda9
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-14 10:10:25 +0200

    Updated task to contain the data from the connection table as well. Added new methods to conDAO

--- src/main/java/com/codecool/web/dao/implementation/TskColSchedConnectorDao.java
+++ src/main/java/com/codecool/web/dao/implementation/TskColSchedConnectorDao.java
@@ -1,5 +1,7 @@
 package com.codecool.web.dao.implementation;
 
+import com.codecool.web.model.Task;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,6 +27,24 @@ class TskColSchedConnectorDao extends AbstractDao {
         return false;
     }
 
+    Task queryTaskConnetions(Task task) throws SQLException {
+        String sql = "SELECT task_id, col_id, schedule_id, task_start, task_end FROM col_tsk WHERE task_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, task.getId());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int colId = resultSet.getInt("col_id");
+                    int scheduleId = resultSet.getInt("schedule_id");
+                    int taskStart = resultSet.getInt("task_start");
+                    int taskEnd = resultSet.getInt("task_end");
+
+                    return fillTask(task, colId, scheduleId, taskStart, taskEnd);
+                }
+            }
+        }
+        return null;
+    }
+
     void insertTask(int taskId, int colId, int scheduleId, int taskStart, int taskEnd) throws SQLException {
         String sql = "INSERT INTO col_tsk (task_id, col_id, schedule_id, task_start, task_end) VALUES (?,?,?,?,?)";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -37,75 +57,56 @@ class TskColSchedConnectorDao extends AbstractDao {
         }
     }
 
-    //Work In Progress!
-
-    void updateTask(int taskId, String colId, String scheduleId, String taskStart, String taskEnd) throws SQLException {
-        /*ConnectorDataTransfer connectorDataTransfer = queryTaskAll(taskId);
-
-        if (colId.equals(""))
-
+    void updateTaskSchedule(int taskId, int colId, int scheduleId, int taskStart, int taskEnd) throws SQLException {
         String sql = "UPDATE col_tsk SET col_id=?, schedule_id=?, task_start=?, task_end=? WHERE task_id=?";
+
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, taskId);
-            statement.setInt(2, colId);
-            statement.setInt(3, scheduleId);
-            statement.setInt(4, taskStart);
-            statement.setInt(5, taskEnd);
+            statement.setInt(5, taskId);
+            statement.setInt(1, colId);
+            statement.setInt(2, scheduleId);
+            statement.setInt(3, taskStart);
+            statement.setInt(4, taskEnd);
             executeInsert(statement);
-        }*/
+        }
     }
 
-    private ConnectorDataTransfer queryTaskAll(int taskId) throws SQLException {
-        String sql = "SELECT task_id, col_id, schedule_id, task_start, task_end FROM col_tsk WHERE task_id = ?";
+    void updateTaskTime(int taskId, int taskStart, int taskEnd) throws SQLException {
+        String sql = "UPDATE col_tsk SET task_start=?, task_end=? WHERE task_id=?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(3, taskId);
+            statement.setInt(1, taskStart);
+            statement.setInt(2, taskEnd);
+            executeInsert(statement);
+        }
+    }
+
+    void updateTaskColumn(int taskId, int colId, int taskStart, int taskEnd) throws SQLException {
+        String sql = "UPDATE col_tsk SET col_id=?, task_start=?, task_end=? WHERE task_id=?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(4, taskId);
+            statement.setInt(1, colId);
+            statement.setInt(2, taskStart);
+            statement.setInt(3, taskEnd);
+            executeInsert(statement);
+        }
+    }
+
+    void deleteTask(int taskId) throws SQLException {
+        String sql = "DELETE FROM col_tsk WHERE task_id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, taskId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    int id = resultSet.getInt("task_id");
-                    int colId = resultSet.getInt("col_id");
-                    int scheduleId = resultSet.getInt("schedule_id");
-                    int taskStart = resultSet.getInt("task_start");
-                    int taskEnd = resultSet.getInt("task_end");
-
-                    return new ConnectorDataTransfer(id, colId, scheduleId, taskStart, taskEnd);
-                }
-            }
+            executeInsert(statement);
         }
-        return null;
     }
 
-    /*Inner class for data transfer of connector table data starts here*/
+    private Task fillTask(Task task, int colId, int scheduleId, int taskStart, int taskEnd) {
+        task.setCol_id(colId);
+        task.setSched_id(scheduleId);
+        task.setStart(taskStart);
+        task.setEnd(taskEnd);
 
-    private class ConnectorDataTransfer {
-
-        int id, colId, scheduleId, taskStart, taskEnd;
-
-        ConnectorDataTransfer(int id, int colId, int scheduleId, int taskStart, int taskEnd) {
-            this.id = id;
-            this.colId = colId;
-            this.scheduleId = scheduleId;
-            this.taskStart = taskStart;
-            this.taskEnd = taskEnd;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public int getColId() {
-            return colId;
-        }
-
-        public int getScheduleId() {
-            return scheduleId;
-        }
-
-        public int getTaskStart() {
-            return taskStart;
-        }
-
-        public int getTaskEnd() {
-            return taskEnd;
-        }
+        return task;
     }
 }
--- src/main/java/com/codecool/web/model/Task.java
+++ src/main/java/com/codecool/web/model/Task.java
@@ -3,7 +3,9 @@ package com.codecool.web.model;
 public class Task {
 
     private final int id;
+    private int col_id, sched_id;
     private String name, content;
+    private int start, end;
 
     public Task(int id, String name, String content) {
         this.id = id;
@@ -24,6 +26,22 @@ public class Task {
         return content;
     }
 
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getCol_id() {
+        return col_id;
+    }
+
+    public int getSched_id() {
+        return sched_id;
+    }
+
     // Setters
     public void setName(String name) {
         this.name = name;
@@ -31,5 +49,21 @@ public class Task {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public void setCol_id(int col_id) {
+        this.col_id = col_id;
+    }
+
+    public void setSched_id(int sched_id) {
+        this.sched_id = sched_id;
     }
 }
