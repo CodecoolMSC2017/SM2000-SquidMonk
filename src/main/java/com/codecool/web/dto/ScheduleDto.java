14486174e29ade1c863374eed9fb1464e61e80e7
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-15 14:44:25 +0200

    Skeleton for schedule page data transfer added

--- src/main/java/com/codecool/web/dto/ScheduleDto.java
+++ src/main/java/com/codecool/web/dto/ScheduleDto.java
@@ -1,9 +1,31 @@
 package com.codecool.web.dto;
 
+import com.codecool.web.model.Column;
+import com.codecool.web.model.Task;
+
 import java.util.List;
 
 public class ScheduleDto {
 
     private int scheduleId;
-    private List<Integer> columnIds;
+    private List<Column> columns;
+    private List<Task> tasks;
+
+    public ScheduleDto(int scheduleId, List<Column> columns, List<Task> tasks) {
+        this.scheduleId = scheduleId;
+        this.columns = columns;
+        this.tasks = tasks;
+    }
+
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
 }
