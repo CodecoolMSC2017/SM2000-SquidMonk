3c33d5931bf7ac5279bd2faa2f04e01cd76e154b
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-05-17 14:42:30 +0200

    Update task looks

--- src/main/webapp/scripts/task.js
+++ src/main/webapp/scripts/task.js
@@ -49,20 +49,26 @@ function onEditButtonClicked() {
     }
     const nameInputEl = document.createElement('input');
     nameInputEl.value = currentTask.name;
+    nameInputEl.placeholder = 'Title';
+    nameInputEl.className = 'task-edit-input';
     nameInputEl.id = 'task-edit-name-input';
 
     const contentInputEl = document.createElement('input');
     contentInputEl.value = currentTask.content;
+    contentInputEl.placeholder = 'Description';
+    contentInputEl.className = 'task-edit-input';
     contentInputEl.id = 'task-edit-content-input';
 
     const submitButton = document.createElement('button');
     submitButton.textContent = 'Save';
+    submitButton.className = 'task-edit-button';
     submitButton.addEventListener('click', onEditTaskSubmitButtonClicked);
 
     const pEl = document.createElement('p');
     pEl.id = 'task-edit-input-container';
     pEl.appendChild(nameInputEl);
     pEl.appendChild(contentInputEl);
+    pEl.appendChild(document.createElement('br'));
     pEl.appendChild(submitButton);
 
     const mainContentEl = document.getElementById('main-content');
@@ -71,6 +77,10 @@ function onEditButtonClicked() {
 
 function createTaskScheduleTable(task) {
     const tableEl = document.createElement('table');
+    tableEl.align = 'center';
+    tableEl.style.marginTop = '20px';
+    tableEl.style.width = '45%';
+    tableEl.className = 'dash-table';
 
     const thEl = document.createElement('th');
     thEl.textContent = 'Schedules containing this task';
@@ -82,6 +92,7 @@ function createTaskScheduleTable(task) {
 
     if (Object.keys(task.schedules).length == 0) {
         const tdEl = document.createElement('td');
+        tdEl.className = 'entry';
         tdEl.textContent = 'This task is not scheduled anywhere.';
 
         const trEl = document.createElement('tr');
@@ -93,6 +104,7 @@ function createTaskScheduleTable(task) {
             const scheduleName = task.schedules[scheduleId];
 
             const tdEl = document.createElement('td');
+            tdEl.className = 'entry';
             tdEl.textContent = scheduleName;
 
             const trEl = document.createElement('tr');
@@ -109,10 +121,12 @@ function createTaskScheduleTable(task) {
 function displayTask(task) {
     currentTask = task;
 
-    const taskNameEl = document.createElement('p');
+    const taskNameEl = document.createElement('h1');
+    taskNameEl.className = 'task-title';
     taskNameEl.textContent = task.name;
 
-    const taskContentEl = document.createElement('p');
+    const taskContentEl = document.createElement('h3');
+    taskContentEl.className = 'task-description';
     if (task.content === '') {
         taskContentEl.textContent = 'No description';
     } else {
@@ -120,20 +134,24 @@ function displayTask(task) {
     }
 
     const scheduleButton = document.createElement('button');
+    scheduleButton.className = 'task-button';
     scheduleButton.textContent = 'Schedule task';
     scheduleButton.addEventListener('click', onScheduleButtonClicked);
 
     const editButton = document.createElement('button');
     editButton.textContent = 'Edit';
+    editButton.className = 'task-button';
     editButton.addEventListener('click', onEditButtonClicked);
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
+    deleteButton.className = 'task-button';
     deleteButton.addEventListener('click', onDeleteButtonClicked);
 
     clearMainContent();
 
     const mainContentEl = document.getElementById('main-content');
+    mainContentEl.style.textAlign = 'center';
     mainContentEl.appendChild(taskNameEl);
     mainContentEl.appendChild(taskContentEl);
     mainContentEl.appendChild(scheduleButton);
