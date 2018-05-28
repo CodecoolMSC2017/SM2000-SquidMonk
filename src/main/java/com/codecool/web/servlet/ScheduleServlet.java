07ad8b26981bf34f4d3033b6671c6e206c59b4d6
Gyorfisti <gyorfisti@gmail.com>
2018-05-28 11:43:26 +0200

    refactor

--- src/main/java/com/codecool/web/servlet/ScheduleServlet.java
+++ src/main/java/com/codecool/web/servlet/ScheduleServlet.java
@@ -4,6 +4,7 @@ import com.codecool.web.dao.ScheduleDao;
 import com.codecool.web.dao.implementation.ScheduleDaoImpl;
 import com.codecool.web.dto.ScheduleDto;
 import com.codecool.web.service.ScheduleService;
+import com.codecool.web.service.exception.ServiceException;
 import com.codecool.web.service.jsService.JsScheduleService;
 
 import javax.servlet.ServletException;
@@ -19,64 +20,80 @@ public class ScheduleServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uri = req.getRequestURI();
-        int schedId = Integer.parseInt(uri.substring(uri.lastIndexOf("/") + 1, uri.length()));
-
         try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleService scheduleService = new JsScheduleService(connection);
 
-            ScheduleDto scheduleDto = scheduleService.fillScheduleDto(schedId);
+            int scheduleId = getScheduleId(req.getRequestURI());
+            ScheduleDto scheduleDto = scheduleService.fillScheduleDto(scheduleId);
             sendMessage(resp, HttpServletResponse.SC_OK, scheduleDto);
         } catch (SQLException e) {
             handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int schedId = Integer.parseInt(req.getParameter("scheduleId"));
-        String columnName = req.getParameter("columnName");
-
         try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleService scheduleService = new JsScheduleService(connection);
 
-            scheduleService.addNewColumnToSchedule(schedId, columnName);
+            int scheduleId = getScheduleId(req.getRequestURI());
+            String columnName = req.getParameter("columnName");
+            scheduleService.addNewColumnToSchedule(scheduleId, columnName);
 
-            ScheduleDto scheduleDto = scheduleService.fillScheduleDto(schedId);
+            ScheduleDto scheduleDto = scheduleService.fillScheduleDto(scheduleId);
             resp.setStatus(HttpServletResponse.SC_OK);
             sendMessage(resp, HttpServletResponse.SC_OK, scheduleDto);
 
         } catch (SQLException e) {
             handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int schedId = Integer.parseInt(req.getParameter("scheduleId"));
 
         try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleService scheduleService = new JsScheduleService(connection);
 
-            scheduleService.deleteSchedule(schedId);
+            int scheduleId = getScheduleId(req.getRequestURI());
+            scheduleService.deleteSchedule(scheduleId);
 
             resp.setStatus(HttpServletResponse.SC_OK);
-            /*sendMessage(resp, HttpServletResponse.SC_OK);*/
-
         } catch (SQLException e) {
             handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uri = req.getRequestURI();
-        int schedId = Integer.parseInt(uri.substring(uri.lastIndexOf("/") + 1, uri.length()));
         try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleDao scheduleDao = new ScheduleDaoImpl(connection);
-            scheduleDao.updateVisibility(schedId);
+
+            int scheduleId = getScheduleId(req.getRequestURI());
+            scheduleDao.updateVisibility(scheduleId);
         } catch (SQLException e) {
             handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    private int getScheduleId(String uri) throws ServiceException {
+        String[] splitUri = uri.split("/");
+        if (uri.length() < 5) {
+            throw new ServiceException("Missing schedule id");
+        }
+        String idAsString = splitUri[4];
+        try {
+            return Integer.parseInt(idAsString);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("Schedule id is not a valid number");
         }
     }
 }
