6292f15d81fd641c26db4136508e6072816a8d02
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-28 13:39:53 +0200

    Fixed order of columns in schedule view, by comparing ids

--- src/main/java/com/codecool/web/dto/ScheduleDto.java
+++ src/main/java/com/codecool/web/dto/ScheduleDto.java
@@ -1,6 +1,10 @@
 package com.codecool.web.dto;
 
+import com.codecool.web.comparator.ScheduleComlumnDtoComparator;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ScheduleDto {
@@ -23,5 +27,9 @@ public class ScheduleDto {
 
     public void addColumns(ScheduleColumnDto columnDto) {
         columns.add(columnDto);
+    }
+
+    public void sortColumnsById() {
+        Collections.sort(columns, new ScheduleComlumnDtoComparator());
     }
 }
