f275979495ecfe5f59691593b8b6c4d4138b498a
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-28 14:14:43 +0200

    Added get method

--- src/main/java/com/codecool/web/servlet/ScheduleTaskServlet.java
+++ src/main/java/com/codecool/web/servlet/ScheduleTaskServlet.java
@@ -1,7 +1,16 @@
 package com.codecool.web.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/protected/schedule/task/*")
 public class ScheduleTaskServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
