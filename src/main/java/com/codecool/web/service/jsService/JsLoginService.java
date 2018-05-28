805c060721ab21b8e55e5704d9384df9eb5636fb
Gyorfisti <gyorfisti@gmail.com>
2018-05-28 15:04:34 +0200

    add basic logging

--- src/main/java/com/codecool/web/service/jsService/JsLoginService.java
+++ src/main/java/com/codecool/web/service/jsService/JsLoginService.java
@@ -5,10 +5,14 @@ import com.codecool.web.model.User;
 import com.codecool.web.service.LoginService;
 import com.codecool.web.service.PassEncrypt;
 import com.codecool.web.service.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class JsLoginService implements LoginService {
+
+    private static final Logger logger = LoggerFactory.getLogger(JsLoginService.class);
 
     private final UserDao userDao;
 
@@ -18,16 +22,19 @@ public class JsLoginService implements LoginService {
 
     @Override
     public User loginUser(String email, String password) throws SQLException, ServiceException {
+        logger.info("logging in user with email [" + email + "]");
         User user = userDao.findByEmail(email);
 
         if (user == null) {
+            logger.info("did not find user with email [" + email + "]");
             throw new ServiceException("Invalid Login!");
         }
         String decryptedPassword = new PassEncrypt().decrypt(user.getPassword());
         if (!decryptedPassword.equals(password)) {
+            logger.info("incorrect password of user with email [" + email + "]");
             throw new ServiceException("Invalid Login!");
         }
-
+        logger.info("login of user with email [" + email + "] was successful");
         return user;
     }
 }
