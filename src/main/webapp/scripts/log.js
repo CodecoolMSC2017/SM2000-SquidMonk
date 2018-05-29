88dbe397480c37cca5b767747852b3ef409cc9b1
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-05-29 16:47:39 +0200

    Added basic log viewing to frontend

---
+++ src/main/webapp/scripts/log.js
@@ -0,0 +1,19 @@
+function onLogReceived() {
+    const log = JSON.parse(this.responseText);
+    const mainDiv = document.getElementById('main-content');
+
+    removeAllChildren(mainDiv);
+
+    const textAreaEl = document.createElement('textarea');
+    
+
+    mainDiv.appendChild(textAreaEl);
+}
+
+function onMenuLogClick() {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onLogReceived);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', '/protected/log');
+    xhr.send();
+}
