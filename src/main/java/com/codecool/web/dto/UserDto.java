f918904846a36b267240094eccdfa8e465d394d6
HajnalFeri <kalacsf1@gmail.com>
2018-05-31 11:49:05 +0200

    set is admin in userdto

--- src/main/java/com/codecool/web/dto/UserDto.java
+++ src/main/java/com/codecool/web/dto/UserDto.java
@@ -1,4 +1,23 @@
 package com.codecool.web.dto;
 
 public class UserDto {
+
+    private int userId;
+    private boolean isAdmin;
+
+    public UserDto(int userId) {
+        this.userId = userId;
+    }
+
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
 }
